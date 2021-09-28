3644f6261fe928d461355cb9210c4759a323f90e
ukaran11 <ukaran11@gmail.com>
2021-09-29 04:47:10 +0530

    Utsav | Added the form and handled submission

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,17 +1,48 @@
 import React from 'react';
-
+import { useFormik } from 'formik';
 const YoutubeForm = () => {
+
+    const formik = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            channel: ''
+        },
+        onSubmit: values => {
+            console.log('Form data', values)
+        }
+    })
+
+    // console.log('Form values', formik.values)
     return (
         <div>
-            <form>
+            <form onSubmit={formik.handleSubmit}>
                 <label htmlFor='name'>Name</label>
-                <input type='text' id='name' name='name' />
+                <input 
+                    type='text' 
+                    id='name' 
+                    name='name' 
+                    onChange={formik.handleChange} 
+                    value={formik.values.name}
+                />
 
                 <label htmlFor='email'>E-mailchannel</label>
-                <input type='email' id='email' name='email' />
+                <input 
+                    type='email' 
+                    id='email' 
+                    name='email' 
+                    onChange={formik.handleChange} 
+                    value={formik.values.email}
+                />
 
                 <label htmlFor='channel'>Channel</label>
-                <input type='text' id='channel' name='channel' />
+                <input 
+                    type='text' 
+                    id='channel' 
+                    name='channel' 
+                    onChange={formik.handleChange} 
+                    value={formik.values.channel}
+                />
 
                 <button>Submit</button>
             </form>
