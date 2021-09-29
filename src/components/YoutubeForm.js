800d92cf28ca374c1b0df640e177dc7422051169
ukaran11 <ukaran11@gmail.com>
2021-09-30 04:36:34 +0530

    Utsav | refactored useFormik hook

--- src/components/YoutubeForm.js
+++ src/components/YoutubeForm.js
@@ -1,16 +1,46 @@
 import React from 'react';
 import { useFormik } from 'formik';
+
+const initialValues = {
+    name: '',
+    email: '',
+    channel: ''
+}
+
+const onSubmit = values => {
+    console.log('Form data', values)
+}
+
+const validate = values => {
+    // values.name values.email values.channel
+    // errors.name errors.email erros.channel
+    // errors.name = 'This field is required'
+
+    let errors = {}
+
+    if(!values.name) {
+        errors.name = 'Required'
+    }
+
+    if(!values.email) {
+        errors.email = 'Required'
+    } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+        errors.email = 'Invalid Email Format'
+    }
+
+    if(!values.channel) {
+        errors.channel = 'Required'
+    }
+
+    return errors
+}
+
 const YoutubeForm = () => {
 
     const formik = useFormik({
-        initialValues: {
-            name: '',
-            email: '',
-            channel: ''
-        },
-        onSubmit: values => {
-            console.log('Form data', values)
-        }
+        initialValues,
+        onSubmit,
+        validate
     })
 
     // console.log('Form values', formik.values)
@@ -44,7 +74,7 @@ const YoutubeForm = () => {
                     value={formik.values.channel}
                 />
 
-                <button>Submit</button>
+                <button type="submit">Submit</button>
             </form>
         </div>
     )
